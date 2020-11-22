df6ab410a7d82bd478e99b044c7d68e1cb415dab
leoportogtr86 <leoporto_studio@hotmail.com>
2020-11-22 13:47:37 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let btn1 = document.getElementById('btn1')
+let btn2 = document.getElementById('btn2')
+let btn3 = document.getElementById('btn3')
+let btn4 = document.getElementById('btn4')
+
+
+btn1.onclick = function () {
+
+    window.location = 'https://leoportogtr86.github.io/domaior/'
+
+}
+
+
+
+btn2.onclick = function () {
+
+    window.location = 'https://5fafd68606e72e307af07108--agitated-kepler-e8b02f.netlify.app/'
+
+}
+
+
+
+btn3.onclick = function () {
+
+    window.location = 'https://5f22a3aa951d4c1837ff153e--leoportojam.netlify.app/'
+
+}
+
+
+
+btn4.onclick = function () {
+
+    window.location = 'https://5faac89435e77c2b77b92133--competent-goldstine-d3c999.netlify.app/'
+
+}
